caf9d6d4acbc2a8e550731bc0e590b824276176e
JS <julienyu@dgist.ac.kr>
2026-04-28 13:39:33 +0900

    Fix: Topic Filter

--- ros2probe/src/cli/topic/hz.rs
+++ ros2probe/src/cli/topic/hz.rs
@@ -9,7 +9,6 @@ use crate::command::protocol::{
 use super::super::bag::util::send_request;
 
 #[derive(Debug, Args)]
-#[command(after_help = "NOTE: Internal topics (tf, rosout, parameter_events, debug topics, SHM-only) are not supported and will not produce output.")]
 pub struct TopicHzCommand {
     /// Name of the ROS topic to observe
     pub topic_name: String,
